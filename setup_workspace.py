c370fdb3b4e423052e82f50363fcf71cc8a107c9
Wilhelm Hedman <hedmanw@student.chalmers.se>
2017-01-20 16:13:22 +0100

    Merge samples are written to files

--- setup_workspace.py
+++ setup_workspace.py
@@ -37,11 +37,22 @@ def main(base, integration, result):
     merge_output = merge(integration)
 
     print "Recorded merging status in README.txt"
+    conflict_files = format_conflicts(merge_output)
     with open('README.txt', 'w') as f:
         f.write(merging_notice + "\n")
         f.write("-------\n")
         f.write("Conflicts:\n")
-        f.write("\n".join(format_conflicts(merge_output)))
+        f.write("\n".join(conflict_files))
+
+    # grep -Pzo Marlin/Marlin.h -e "(?s)<<<<<<< HEAD.*if.*>>>>>>>"
+    # grep -Pzo Marlin/Marlin_main.cpp -e "(?s)<<<<<<< HEAD.*?ifdef.*?>>>>>>>"
+    # awk '/<<<<<<< HEAD/,/>>>>>>>/' Marlin/Marlin_main.cpp
+    # TODO: pipe all files in merge_output into awk and then into grep(?)
+    # TODO: split output on merge characters
+    # TODO: trace back to original file in order to extract line numbers
+    # TODO: write metadata (filename, line numbers) and code to files
+
+    # TODO: manually check how the merge was resolved
 
 
 def clone(repository):
@@ -66,6 +77,34 @@ def merge(commit):
     return None
 
 
+def sample_merge(files):
+    merge_limits = map(lambda x: awk(x), files)
+    files_to_conflicts = {}
+    for index, source_file in enumerate(files):
+        files_to_conflicts[source_file] = merge_limits[index].split(">>>>>>>")
+
+    return files_to_conflicts
+
+
+def print_samples(files_with_conflicts):
+    counter = 0
+    for source_file in files_with_conflicts:
+        all_conflicts = files_with_conflicts[source_file]
+        for conflict in all_conflicts:
+            with open("{}_{}".format(source_file[source_file.index('/')+1:], counter), 'w') as f:
+                f.write("// EXCERPT FROM {}\n\n".format(source_file))
+                f.write(conflict + "\n")
+            counter += 1
+
+
+def awk(file_path):
+    awk_process = Popen(["awk", "/<<<<<<< HEAD/,/>>>>>>>/", file_path], cwd=workspace_dir, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = awk_process.communicate()
+    awk_process.wait()
+    print stderr
+    return stdout
+
+
 def git_process(args, cwd=None):
     args.insert(0, "git")
     process = Popen(args, cwd=cwd, stdout=PIPE, stderr=PIPE)
@@ -80,6 +119,13 @@ def format_conflicts(merge_text):
     only_filenames = lambda x: x[len("CONFLICT (content): Merge conflict in "):]
     return map(only_filenames, filter(relevant_files, filter(only_conflict_lines, merge_lines)))
 
+conflicts_sample = """Marlin/ultralcd_implementation_hitachi_HD44780.h
+Marlin/ultralcd.cpp
+Marlin/pins.h
+Marlin/language.h
+Marlin/Marlin_main.cpp
+Marlin/Configuration.h"""
+
 
 if __name__ == '__main__':
     base_repo = Repo("fsantini", "solidoodle2-marlin")
@@ -88,4 +134,7 @@ if __name__ == '__main__':
 
     integration_repo = Repo("MarlinFirmware", "Marlin")
     integration = Commit(integration_repo, "d75cd69de43afada517557b63a6c693eaa828580")
-    main(base, integration, result)
+    #main(base, integration, result)
+    sample = sample_merge(conflicts_sample.splitlines())
+    print_samples(sample)
+    #print awk("Marlin/Marlin_main.cpp")
