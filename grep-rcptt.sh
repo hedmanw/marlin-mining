795c67fc2ff138a7a8b71231a63bdfdeb36602be
Wilhelm Hedman <w.hedman@gmail.com>
2017-03-09 06:41:29 +0100

    Updated grep rcptt script

--- grep-rcptt.sh
+++ grep-rcptt.sh
@@ -1,25 +1,31 @@
-echo $1
-echo "copy:"
-grep $1 -e "M1+c" | wc -l
-echo "paste:"
-grep $1 -e "M1+v" | wc -l
-echo "undo:"
-grep $1 -e "M1+z" | wc -l
-echo "Delete:"
-grep $1 -e "Del" | wc -l
-echo "Backspace:"
-grep $1 -e "BackSpace" | wc -l
+#!/usr/bin/env bash
+
+#echo $1
+#echo "copy:"
+#grep $1 -e "M1+c" | wc -l
+#echo "paste:"
+#grep $1 -e "M1+v" | wc -l
+#echo "undo:"
+#grep $1 -e "M1+z" | wc -l
+#echo "Delete:"
+#grep $1 -e "Del" | wc -l
+#echo "Backspace:"
+#grep $1 -e "BackSpace" | wc -l
 echo "manual ifdef"
 grep $1 -e "#if" | wc -l
-grep $1 -e "else" | wc -l
-grep $1 -e "endif" | wc -l
-echo "Copy all"
-grep $1 -e "Copy All" | wc -l
-echo "Copy current"
-grep $1 -e "Copy Current" | wc -l
-echo "Enter"
-grep $1 -e "key-type Enter" | wc -l
-echo "Tab"
-grep $1 -e "key-type Tab" | wc -l
-grep $1 -e "type-text"
-grep $1 -e "type-text" | wc -l
+grep $1 -e "#if"
+#grep $1 -e "else" | wc -l
+#grep $1 -e "endif" | wc -l
+#echo "Copy all"
+#grep $1 -e "Copy All" | wc -l
+#echo "Copy current"
+#grep $1 -e "Copy Current" | wc -l
+#echo "Enter"
+#grep $1 -e "key-type Enter" | wc -l
+#echo "Tab"
+#grep $1 -e "key-type Tab" | wc -l
+#grep $1 -e "type-text"
+#grep $1 -e "type-text" | wc -l
+echo "manual ifdef w token"
+grep $1 -E -e "#if.* .*" | wc -l
+grep $1 -E -e "#if.* .*"
